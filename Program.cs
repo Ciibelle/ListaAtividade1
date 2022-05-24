ca51debdd404777589e973682cea87662ae71322
Ciibelle <cibellegon6565@gmail.com>
2022-05-24 20:32:27 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Atividade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mediaMin = 20;
+            int mediaMax = 50;
+            int mediaAluno = 0;
+
+            mediaAluno = (mediaMin + mediaMax)/2;
+
+            Console.WriteLine($"A idade média dos alunos é: {mediaAluno} anos");
+        }
+    }
+}
